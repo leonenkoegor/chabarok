3a8281df93dae3f1c5bb5482eca2f703e5421441
Nick <nick>
2020-06-18 02:08:04 +0300

    Валидация на вход

--- src/main/resources/static/js/signIn.js
+++ src/main/resources/static/js/signIn.js
@@ -1,5 +1,8 @@
 let url = window.location.href;
-
+let signInForm = document.getElementById('signInForm');
 if(url === 'http://localhost:8080/login?error'){
-
+    let signError = document.createElement('div');
+    signError.className = 'signInError';
+    signError.textContent = 'Неверный логин или пароль';
+    signInForm.append(signError);
 }
