ad78e3bfad3c66bbd228c040aa9a5abe4a7e2053
leon <leonenko4egor@gmail.com>
2020-06-17 21:12:35 +0000

    Initial commit

---
+++ src/main/java/codes/purple/chabarok/ChabarokApplication.java
@@ -0,0 +1,13 @@
+package codes.purple.chabarok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChabarokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChabarokApplication.class, args);
+	}
+
+}
