f34143adfedddaf2836d8a053bb66336849f8ab1
leon <leonenko4egor@gmail.com>
2020-06-17 22:22:39 +0000

    add user repository

---
+++ src/main/java/codes/purple/chabarok/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package codes.purple.chabarok.repositories;
+
+import codes.purple.chabarok.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
+}
