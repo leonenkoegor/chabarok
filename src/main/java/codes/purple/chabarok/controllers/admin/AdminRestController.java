f05ad8982c04566d8044d943ca95289bdbe84765
leon <leonenko4egor@gmail.com>
2020-06-18 18:18:50 +0000

    optimize imports

--- src/main/java/codes/purple/chabarok/controllers/admin/AdminRestController.java
+++ src/main/java/codes/purple/chabarok/controllers/admin/AdminRestController.java
@@ -9,7 +9,6 @@ import codes.purple.chabarok.services.exceptions.ShortUsernameException;
 import codes.purple.chabarok.services.exceptions.UserAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
