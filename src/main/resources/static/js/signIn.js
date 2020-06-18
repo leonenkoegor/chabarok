6ad456635d501ef1ef12131b1a8d27565f9a0faf
Nick <nick>
2020-06-18 03:13:31 +0300

    Авторизация + Logout

--- src/main/resources/static/js/signIn.js
+++ src/main/resources/static/js/signIn.js
@@ -1,8 +1,31 @@
 let url = window.location.href;
+let password = document.getElementById('exampleInputPassword1');
+let login = document.getElementById('login');
 let signInForm = document.getElementById('signInForm');
+let signBtn = document.getElementById('btn');
 if(url === 'http://localhost:8080/login?error'){
-    let signError = document.createElement('div');
-    signError.className = 'signInError';
-    signError.textContent = 'Неверный логин или пароль';
-    signInForm.append(signError);
+    validation(
+        'Неверный логин или пароль'
+    )
+};
+signBtn.addEventListener('click',function (event) {
+    event.preventDefault();
+    if(password.value.length<6 || login.value.length<6){
+        validation(
+            'Поля ввода должны содержать не менее 6 символов'
+        )
+    }else{
+        signInForm.submit();
+    }
+});
+
+function validation(text) {
+    if(signBtn.nextElementSibling == undefined) {
+        let signError = document.createElement('div');
+        signError.className = 'signInError';
+        signError.textContent = text;
+        signInForm.append(signError);
+    }else{
+        signBtn.nextElementSibling.textContent = 'Поля ввода должны содержать не менее 6 символов';
+    }
 }
