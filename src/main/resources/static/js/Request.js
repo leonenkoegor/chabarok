8f7be8f5389ce47ca395563271bd919fc0dc215a
leon <leonenko4egor@gmail.com>
2020-06-18 18:15:27 +0000

    refactor

--- src/main/resources/static/js/Request.js
+++ src/main/resources/static/js/Request.js
@@ -1,42 +1,40 @@
 export default class Request {
-    constructor(method, url, params,xhrType = null) {
+    constructor(method, url, data, isJSON = true) {
         this.method = method;
         this.url = url;
-        this.params = params;
-        this.xhrType = xhrType;
+        this.data = data;
+        this.isJSON = isJSON;
     }
 
     sendRequest(callback) {
         const xhr = new XMLHttpRequest()
 
-        if ((this.method === 'GET')) {
-            let body = "?";
-            this.params.forEach((value) => {
-                body += value + "&";
-            })
-            body = body.substring(0, body.length - 1);
-            xhr.open(this.method, this.url + body);
-        } else {
-            xhr.open(this.method, this.url)
-        }
-
-        if(this.xhrType === null){
+        if (this.isJSON === true) {
             xhr.responseType = 'json';
         }
+
         xhr.onload = () => {
             callback(xhr.response);
         }
 
-        if (this.method === 'POST' && typeof this.params !== "string") {
-            let body = "";
-            this.params.forEach((value) => {
-                body += value + "&";
-            })
-            body = body.substring(0, body.length - 1);
-            xhr.send(body);
+        if (typeof this.data === "string") {
+            xhr.open(this.method, this.url)
+            xhr.send(this.data);
+            return;
+        }
+
+        let body = "?";
+        for (let key in this.data) {
+            body += key + "=" + this.data[key] + "&";
+        }
+        body = body.substring(0, body.length - 1);
+
+        if (this.method === "GET") {
+            xhr.open(this.method, this.url + body);
+            xhr.send();
         } else {
-            xhr.send(this.params);
+            xhr.open(this.method, this.url);
+            xhr.send(body);
         }
     }
-
 }
