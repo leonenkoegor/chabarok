fa84d76e83ff2a2356dc4be498d3d77d342c3ef1
Nick <nick>
2020-08-29 23:32:27 +0300

    Главная страница обновление 2.1

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,22 @@
+function initMap() {
+    let myLatlng = new google.maps.LatLng(52.345241, 31.029524);
+    let mapOptions = {
+        zoom: 16,
+        center: myLatlng
+    }
+
+    let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    let info = new google.maps.InfoWindow({
+        content: '<h3 id="mapToastHeader">Ресторан чабарок</h3><p id="mapToastDesc">Мы ждём вас!</p>'
+    });
+
+    let marker = new google.maps.Marker({
+        position: myLatlng,
+        title: "Добро пожаловать в Чабарок!",
+    });
+    marker.addListener('click', function () {
+        info.open(map, marker);
+    })
+    marker.setMap(map);
+// To add the marker to the map, call setMap();
+}
