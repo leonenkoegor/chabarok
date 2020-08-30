5bf2030e1b6145c309f9fbf9dc8eb8ab01754dc5
leon <local@local>
2020-08-31 02:30:44 +0300

    azazaza

--- src/main/java/codes/purple/chabarok/controllers/GuestRestController.java
+++ src/main/java/codes/purple/chabarok/controllers/GuestRestController.java
@@ -3,11 +3,13 @@ package codes.purple.chabarok.controllers;
 import codes.purple.chabarok.controllers.responses.DataResponse;
 import codes.purple.chabarok.controllers.responses.Status;
 import codes.purple.chabarok.dtos.OrderedTableDTO;
+import codes.purple.chabarok.models.Dish;
 import codes.purple.chabarok.models.OrderedTable;
 import codes.purple.chabarok.services.CategoryService;
 import codes.purple.chabarok.services.DishService;
 import codes.purple.chabarok.services.OrderedTableService;
 import codes.purple.chabarok.services.exceptions.CategoryNotFoundException;
+import codes.purple.chabarok.services.exceptions.DishNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,5 +59,18 @@ public class GuestRestController {
         } catch (CategoryNotFoundException e) {
             return new DataResponse(Status.FAIL, "Category not found!", null);
         }
+    }
+
+    @GetMapping("/order/dishes/get")
+    public DataResponse getDishes(@RequestParam List<Long> dishes) {
+        List<Dish> dishesList = new LinkedList<>();
+        for(Long dish: dishes) {
+            try {
+                dishesList.add(dishService.findById(dish));
+            } catch (DishNotFoundException e) {
+                return new DataResponse(Status.FAIL, "Dish not found", null);
+            }
+        }
+        return new DataResponse(Status.SUCCESS, "Dishes by id", dishesList);
     }
 }
