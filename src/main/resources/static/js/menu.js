6f81f4fbf55893e8572e2703c0f88ab3f6401ae9
Nick <nick>
2020-08-30 20:02:48 +0300

    меню

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,6 +1,7 @@
 import Request from "./Request.js"
-let categories = document.querySelectorAll('.subMenu li');
-let menu = document.getElementById('categories');
+let menu1 = document.getElementById('hootFood');
+let menu2 = document.getElementById('coldFood');
+let menu3 = document.getElementById('drinks');
 let getCategories = new Request('GET','/menu/categories/get');
 
 getCategories.sendRequest(function (xhr) {
@@ -10,19 +11,26 @@ getCategories.sendRequest(function (xhr) {
             let category = document.createElement('li');
             category.setAttribute('subId',categoryList[i]["id"]);
             category.textContent = categoryList[i]["name"];
-            menu.append(category);
-        }
-})
- for(let i=0;i<categories.length;i++){
-     categories[i].addEventListener('click',function () {
-
-        let request = new Request('GET','/menu/dishes/get?categoryId'+categories[i].getAttribute('subId'));
-           request.sendRequest(function (xhr) {
-          if(xhr.status ==='SUCCESS'){
-                console.log(xhr);
+            if(categoryList[i]["mainCategoryName"] === 'Горячие блюда'){
+                menu1.append(category);
+            }else if(categoryList[i]["mainCategoryName"] === 'Холодные закуски'){
+                menu2.append(category);
             }else{
+                menu3.append(category);
+            }
+            category.addEventListener('click',function () {
+                let request = new Request('GET','/menu/dishes/get?categoryId'+category.getAttribute('subId'));
+                request.sendRequest(function (xhr) {
+                    if(xhr.status ==='SUCCESS'){
+                        console.log(xhr);
+                    }else{
 
-             }
-         })
-     })
- }
+                    }
+                })
+            })
+
+        }
+
+})
+
+
