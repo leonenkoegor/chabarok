fe4bff415888fd843e460c35ee049b1047d2c80b
Nick <nick>
2020-08-30 20:55:56 +0300

    меню

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -1,7 +1,8 @@
 import Request from "./Request.js"
-let menu1 = document.getElementById('hootFood');
+let menu1 = document.getElementById('hotFood');
 let menu2 = document.getElementById('coldFood');
 let menu3 = document.getElementById('drinks');
+let cardContainer = document.getElementById('cardContainer');
 let getCategories = new Request('GET','/menu/categories/get');
 
 getCategories.sendRequest(function (xhr) {
@@ -19,10 +20,30 @@ getCategories.sendRequest(function (xhr) {
                 menu3.append(category);
             }
             category.addEventListener('click',function () {
-                let request = new Request('GET','/menu/dishes/get?categoryId'+category.getAttribute('subId'));
+                let request = new Request('GET','/menu/dishes/get?categoryId='+category.getAttribute('subId'));
                 request.sendRequest(function (xhr) {
                     if(xhr.status ==='SUCCESS'){
-                        console.log(xhr);
+                        cardContainer.innerHTML = '';
+                        let foodList = xhr.data;
+                        for (let i=0;i<foodList.length;i++){
+                        let card = document.createElement('div');
+                        card.classList.add('card');
+
+                        card.setAttribute('subId',foodList[i]["id"]);
+                        card.innerHTML = `
+                        <div class="card-img"></div>
+                <h3>${foodList[i]["name"]}
+                </h3>
+                <p class="foodDescription">${foodList[i]["description"]}</p>
+                <div class="foodParamContainer">
+                    <div class="weight">${foodList[i]["weight"]}</div>
+                    <div class="price">${foodList[i]["cost"]}</div>
+                </div>
+                <div class="addToCartBtn">Добавить в корзину</div>
+            </div>
+                  `
+                            cardContainer.append(card);
+                        }
                     }else{
 
                     }
