67ad8da3070d30069ab9649dd8534a18ac93ccf3
leon <local@local>
2020-08-31 03:09:03 +0300

    azazaza

--- src/main/java/codes/purple/chabarok/repositories/OrderedTableRepository.java
+++ src/main/java/codes/purple/chabarok/repositories/OrderedTableRepository.java
@@ -4,7 +4,7 @@ import codes.purple.chabarok.models.OrderedTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 @Repository
