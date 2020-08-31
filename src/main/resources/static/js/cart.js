c6b256dbb2a0a99e11b5679a6f80ad32c22df8ee
Nick <nick>
2020-08-31 03:01:08 +0300

    Добавление блюд

---
+++ src/main/resources/static/js/cart.js
@@ -0,0 +1,21 @@
+import Request from "./Request.js"
+
+
+
+   let getData = JSON.parse(localStorage.getItem('cart'));
+    let listArray = getData.list;
+    let list ='';
+    for (let i=0;i<listArray.length;i++){
+            list += 'dishes=' + listArray[i] + '&';
+    }
+    list = list.substring(0, list.length - 1);
+    console.log(list);
+
+    let request = new Request('GET','/cart/dishes/get?' + list);
+    request.sendRequest(function (xhr) {
+        if(xhr.status ==='SUCCESS'){
+            console.log(xhr);
+        }else{
+
+        }
+    })
