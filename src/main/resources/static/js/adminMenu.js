5f95c09352f9c813296e9b3dbe130f9eebbbc5c3
Nick <nick>
2020-09-09 21:59:59 +0300

    Много чего нового

--- src/main/resources/static/js/adminMenu.js
+++ src/main/resources/static/js/adminMenu.js
@@ -2,6 +2,21 @@ import Request from "./Request.js"
 let select = document.getElementById('select');
 let select2 = document.getElementById('select2');
 let getCategories = new Request('GET','/menu/categories/get');
+let addSubCategory = document.getElementById('addSubCategory');
+addSubCategory.addEventListener('click',sendRequestForAddSubCategory);
+
+function sendRequestForAddSubCategory() {
+    let send = {
+        'mainCategoryName':document.getElementById('mainCategoryName'),
+        'name':document.getElementById('subCategory')
+    }
+    let addSubCategoryRequest = new Request('GET', '/admin/menu/category/add', send);
+    addSubCategoryRequest.sendRequest(function (xhr) {
+        if(xhr.status ==='SUCCESS'){
+            console.log('gugi');
+        }
+    })
+}
 getCategories.sendRequest(function (xhr) {
     console.log(xhr);
     let categoryList = xhr.data;
