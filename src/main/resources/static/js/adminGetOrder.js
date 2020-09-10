efdde27599f04f1470c7e548aeb8fc3a11ea67cc
Nick <nick>
2020-09-10 13:08:38 +0300

    готов

--- src/main/resources/static/js/adminGetOrder.js
+++ src/main/resources/static/js/adminGetOrder.js
@@ -1,7 +1,44 @@
 import Request from "./Request.js";
+let message = document.getElementById('message');
+let date = document.getElementById('date');
+let row = document.getElementById('row');
+setInterval(function () {
+    if (date.value){
+    let getOrder = new Request('GET', '/admin/orders/get?date=' + date.value);
+    row.innerHTML = '';
+    getOrder.sendRequest(function (xhr) {
+        if (xhr["data"] === undefined || xhr["data"].length === 0) {
+            console.log(xhr["data"]);
+            message.style.display = 'block';
+        } else {
+            message.style.display = 'none';
+            for (let i = 0; i < xhr["data"].length; i++) {
+                let order = document.createElement('div');
+                order.className = 'col-md-4';
+                order.innerHTML = `
+            <div class="card">
+             <div class="card-body border rounded-0">
+            <h5 class="text-left card-title">Заказ № ${xhr["data"][i]["id"]}</h5>
+            <h5 class="text-left card-title">Фамилия и имя: ${xhr["data"][i]["name"]}</h5>
+            <h5 class="text-left card-title">Номер телефона: ${xhr["data"][i]["phoneNumber"]}</h5>
+            <h5 class="text-left card-title">Дата: ${xhr["data"][i]["date"]}</h5
+            <h5 class="text-left card-title">Цена:${xhr["data"][i]["totalPrice"]}</h5>
+            <h5 class="text-left card-title">Адрес:${xhr["data"][i]["address"]}</h5>
+            <h5 class="text-left card-title">Способ оплаты:${xhr["data"][i]["isToPayByCard"] ? ' картой' : ' наличными'}</h5>
+            </div>
+            </div>
+           
+            `
+                row.append(order);
+            }
+        }
 
+    })}
+},30000)
 function dateCorrected() {
-
+    let message = document.getElementById('message');
+    let date = document.getElementById('date');
+    let row = document.getElementById('row');
     if (this.value.length === 2) {
         if (this.value[1] != '/') {
             this.value += '/';
@@ -17,9 +54,6 @@ function dateCorrected() {
         }
     }
 }
-let message = document.getElementById('message');
-let date = document.getElementById('date');
-let row = document.getElementById('row');
 date.addEventListener('input', dateCorrected);
 date.addEventListener('blur', function () {
     let getOrder = new Request('GET', '/admin/orders/get?date=' + this.value);
@@ -36,17 +70,18 @@ date.addEventListener('blur', function () {
                 order.innerHTML = `
             <div class="card">
              <div class="card-body border rounded-0">
-            <h5 class="text-right card-title">Заказ № ${xhr["data"][i]["id"]}</h5>
-            <h5 class="text-right card-title">Фамилия и имя: ${xhr["data"][i]["name"]}</h5>
-            <h5 class="text-right card-title">Номер телефона: ${xhr["data"][i]["phoneNumber"]}</h5>
-            <h5 class="text-right card-title">Дата: ${xhr["data"][i]["orderedDate"]}</h5>
-            <h5 class="text-right card-title mb-2">Время с:${xhr["data"][i]["orderedFromTime"]}</h5>
-            <h5 class="text-right card-title mb-2">Количество гостей:${xhr["data"][i]["peoples"]}</h5>
+            <h5 class="text-left card-title">Заказ № ${xhr["data"][i]["id"]}</h5>
+            <h5 class="text-left card-title">Фамилия и имя: ${xhr["data"][i]["name"]}</h5>
+            <h5 class="text-left card-title">Номер телефона: ${xhr["data"][i]["phoneNumber"]}</h5>
+            <h5 class="text-left card-title">Дата: ${xhr["data"][i]["date"]}</h5
+            <h5 class="text-left card-title">Цена:${xhr["data"][i]["totalPrice"]}</h5>
+            <h5 class="text-left card-title">Адрес:${xhr["data"][i]["address"]}</h5>
+            <h5 class="text-left card-title">Способ оплаты:${xhr["data"][i]["isToPayByCard"]?' картой':' наличными'}</h5>
             </div>
             </div>
            
             `
-                row.append(booking);
+                row.append(order);
             }
         }
     })
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,20 +1,24 @@
 import Request from "./Request.js"
 
 let eventsContainer = document.getElementById('events');
-let getEvents = new Request('GET','/admin/events/get');
+let getEvents = new Request('GET','/events/get');
 
 
 
 getEvents.sendRequest(function (xhr) {
-    if(xhr.status === 'SUCCESS'){
-        for(let i=0;i<xhr["data"].length;i++){
-            let url = `/menu/dishes/image/get?dishId=${xhr["data"][i]["id"]}`;
-            let event = document.createElement('div');
-            event.className = 'event-content';
-            event.innerHTML = `
+    if(xhr !=null) {
+        if (xhr.status === 'SUCCESS') {
+            eventsContainer.innerHTML = '';
+            for (let i = 0; i < xhr["data"].length; i++) {
+                let url = `/events/image/get?eventId=${xhr["data"][i]["id"]}`;
+                let event = document.createElement('div');
+                event.className = 'event-content';
+                event.innerHTML = `
             <div class="photo" style="background-image: url(${url})"></div>
                 <p class="photo-desc">${xhr["data"][i]["description"]}</p>
             `
+                eventsContainer.append(event);
+            }
         }
     }
 })
